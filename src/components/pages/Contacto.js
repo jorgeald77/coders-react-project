0e8eae59b1ec989e91ff73d2a37719109ab0215d
Jorge G <jorgeald77@gmail.com>
2022-10-15 21:01:14 -0500

    Se creo la funcionalidad para guardar en Firestore las dudas de los usuarios

--- src/components/pages/Contacto.js
+++ src/components/pages/Contacto.js
@@ -1,6 +1,21 @@
+import {useState} from "react";
+import {addDoc, collection} from "firebase/firestore";
+import {db} from "../../utils/firebase";
+
 export default function Contacto() {
-    // TODO crear tabla para guardar mensajes
-    // Procesar el envio a la DB y mostrar valiación de recibo
+    const [folio, setFolio] = useState('')
+
+    const handlerSubmit = (event) => {
+        event.preventDefault();
+        let duda = {
+            'nombre': event.target[0].value,
+            'email': event.target[1].value,
+            'mensaje': event.target[2].value
+        }
+
+        addDoc(collection(db, 'dudas'), duda)
+            .then(({id}) => setFolio(id))
+    }
 
     return (
         <div>
@@ -10,7 +25,7 @@ export default function Contacto() {
             </div>
 
             <div className='mx-auto mt-10 w-1/2'>
-                <form id='form-contacto'>
+                <form id='form-contacto' onSubmit={handlerSubmit}>
                     <div className='overflow-hidden shadow rounded'>
                         <div className='px-4 py-5 border-2 border-stone-700 rounded'>
                             <div className='grid grid-cols-6 gap-6'>
@@ -24,11 +39,21 @@ export default function Contacto() {
                                 </div>
                                 <div className='col-span-6'>
                                     <label htmlFor="mensaje">Mensaje</label>
-                                    <textarea id='mensaje' name='mensaje' reqired></textarea>
+                                    <textarea id='mensaje' name='mensaje' required></textarea>
                                 </div>
-                                <div className='col-span-6'>
-                                    <input type="submit" className='btn-info' value='Enviar'/>
-                                </div>
+
+                                {
+                                    folio == ""
+                                        ?
+                                        <div className='col-span-6'>
+                                            <button type="submit" className='btn-info'>Enviar</button>
+                                        </div>
+                                        :
+                                        <div className='col-span-6 font-semibold'>
+                                            Hemos recibido su duda, pronto nos contactaremos a su correo.
+                                            <br/> {folio}
+                                        </div>
+                                }
                             </div>
                         </div>
                     </div>
